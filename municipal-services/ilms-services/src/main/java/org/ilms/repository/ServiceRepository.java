95454482c8dba8be06c5abdc1b1e429f21c74581
Manmohan-si <manmohan.singh@walkingtree.tech>
2022-12-29 14:07:06 +0530

    Added user response

--- municipal-services/ilms-services/src/main/java/org/ilms/repository/ServiceRepository.java
+++ municipal-services/ilms-services/src/main/java/org/ilms/repository/ServiceRepository.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Optional;
 import org.egov.tracer.model.CustomException;
 import org.egov.tracer.model.ServiceCallException;
+import org.ilms.web.model.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.HttpClientErrorException;
@@ -44,14 +45,14 @@ public class ServiceRepository {
         return Optional.ofNullable(response);
     }
 
-    public Object fetchUserResult(StringBuilder uri, Object request) {
+    public UserResponse fetchUserResult(StringBuilder uri, Object request) {
 
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        Object response = null;
+        UserResponse response = null;
         log.info("URI: " + uri.toString());
         try {
             log.info("Request: " + mapper.writeValueAsString(request));
-            response = restTemplate.postForObject(uri.toString(), request, Map.class);
+            response = restTemplate.postForObject(uri.toString(), request, UserResponse.class);
         } catch (HttpClientErrorException e) {
 
             log.error("External Service threw an Exception: ", e);
