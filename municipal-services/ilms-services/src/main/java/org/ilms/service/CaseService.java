f6b3e1a7f959ca8666ae7543ff4464a2bf617212
VaibhavJain1996 <vaibhav.jain@walkingtree.tech>
2023-01-03 16:50:26 +0530

    Update CaseService.java

--- municipal-services/ilms-services/src/main/java/org/ilms/service/CaseService.java
+++ municipal-services/ilms-services/src/main/java/org/ilms/service/CaseService.java
@@ -219,7 +219,7 @@ public class CaseService {
                 //                todo : notification has send to all the officers who has worked on this case.
                 if (Objects.nonNull(caseRequest.getCases().getWorkflow())) {
                     processCaseUpdate(caseRequest, updatedCaseRequest.getCases());
-            //        notificationService.process(ilmsConfiguration.getUpdateCaseTopic(),caseRequest);
+                    notificationService.process(ilmsConfiguration.getUpdateCaseTopic(),caseRequest);
                 }
             } else {
                 throw new CustomException(ILMSErrorConstants.CASE_NOT_AVAILABLE, "Case is not Available");
