7281dd5765bf307b0c759cab30d1791f4a871d9f
Shiva Amoria <shiva.amoria@walkingtree.tech>
2023-01-11 16:48:04 +0530

    Update HearingService.java

--- municipal-services/ilms-services/src/main/java/org/ilms/service/HearingService.java
+++ municipal-services/ilms-services/src/main/java/org/ilms/service/HearingService.java
@@ -58,50 +58,58 @@ public class HearingService {
                 hearingRequest.getHearing().setStatus(Status.ACTIVE);
                 if (Objects.nonNull(hearingRequest.getHearing().getCourt())) {
                     hearingRequest.getHearing().getCourt().setStatus(Status.ACTIVE);
-                }
-
-                if (Objects.nonNull(hearingRequest.getHearing().getPetitioner())) {
-                    hearingRequest.getHearing().getPetitioner().setCaseId(hearingRequest.getHearing().getCaseId());
-                    hearingRequest.getHearing().getPetitioner().setPartyType(PartyType.PETITIONER.toString());
-                    hearingRequest.getHearing().getPetitioner().setStatus(Status.ACTIVE);
-                    if (Objects.nonNull(hearingRequest.getHearing().getPetitioner().getAdvocate())) {
-                        hearingRequest.getHearing().getPetitioner().getAdvocate().setPartyType(PartyType.PETITIONER);
-                        hearingRequest.getHearing().getPetitioner().getAdvocate().setStatus(Status.ACTIVE);
-                    }
                 } else {
-                    for (Party party : partyList) {
-                        if (party.getPartyType().equals(PartyType.PETITIONER.toString())) {
-                            hearingRequest.getHearing().setPetitioner(party);
-                        }
-                    }
-                    if (Objects.nonNull(hearingRequest.getHearing().getRespondent())) {
-                        hearingRequest.getHearing().getRespondent().setCaseId(hearingRequest.getHearing().getCaseId());
-                        hearingRequest.getHearing().getRespondent().setStatus(Status.ACTIVE);
-                        hearingRequest.getHearing().getRespondent().setPartyType(PartyType.RESPONDENT.toString());
-                        if (Objects.nonNull(hearingRequest.getHearing().getRespondent().getAdvocate())) {
-                            hearingRequest.getHearing().getRespondent().getAdvocate().setPartyType(PartyType.RESPONDENT);
-                            hearingRequest.getHearing().getRespondent().getAdvocate().setStatus(Status.ACTIVE);
-                        }
-                    } else {
-                        for (Party party : partyList) {
-                            if (party.getPartyType().equals(PartyType.RESPONDENT.toString())) {
-                                hearingRequest.getHearing().setRespondent(party);
+                    hearingRequest.getHearing().getCourt().setStatus(Status.DRAFTED);
+                }
+                for (Party party : partyList) {
+                    if (party.getPartyType().equals(PartyType.PETITIONER.toString())) {
+                        if (Objects.nonNull(hearingRequest.getHearing().getPetitioner())) {
+                            hearingRequest.getHearing().getPetitioner().setCaseId(hearingRequest.getHearing().getCaseId());
+                            hearingRequest.getHearing().getPetitioner().setPartyType(PartyType.PETITIONER.toString());
+                            hearingRequest.getHearing().getPetitioner().setStatus(Status.ACTIVE);
+                            if (Objects.nonNull(hearingRequest.getHearing().getPetitioner().getAdvocate())) {
+                                hearingRequest.getHearing().getPetitioner().getAdvocate().setPartyType(PartyType.PETITIONER);
+                                hearingRequest.getHearing().getPetitioner().getAdvocate().setStatus(Status.ACTIVE);
+                                hearingRequest.getHearing().getPetitioner().getAdvocate().setPartyId(party.getId());
+
                             }
+                        } else {
+                            Party partyPet = new Party();
+                            partyPet.setAdvocate(party.getAdvocate());
+                            hearingRequest.getHearing().setPetitioner(partyPet);
                         }
                     }
-                    if (Objects.nonNull(hearingRequest.getHearing().getPayment())) {
-                        hearingRequest.getHearing().getPayment().setStatus(Status.ACTIVE);
+                    if (party.getPartyType().equals(PartyType.RESPONDENT.toString())) {
+                        if (Objects.nonNull(hearingRequest.getHearing().getRespondent())) {
+                            hearingRequest.getHearing().getRespondent().setCaseId(hearingRequest.getHearing().getCaseId());
+                            hearingRequest.getHearing().getRespondent().setStatus(Status.ACTIVE);
+                            hearingRequest.getHearing().getRespondent().setPartyType(PartyType.RESPONDENT.toString());
+                            if (Objects.nonNull(hearingRequest.getHearing().getRespondent().getAdvocate())) {
+                                hearingRequest.getHearing().getRespondent().getAdvocate().setPartyType(PartyType.RESPONDENT);
+                                hearingRequest.getHearing().getRespondent().getAdvocate().setStatus(Status.ACTIVE);
+                                hearingRequest.getHearing().getRespondent().getAdvocate().setPartyId(party.getId());
+                            }
+                        } else {
+                            Party partyRes = new Party();
+                            partyRes.setAdvocate(party.getAdvocate());
+                            hearingRequest.getHearing().setRespondent(partyRes);
+                        }
                     }
-                    hearingRequest.getHearing().setHearingNumber(hearingDetailsRepository.getMaxValueOfHearing(hearingRequest.getHearing().getCaseId()));
                 }
 
-
+                if (Objects.nonNull(hearingRequest.getHearing().getPayment())) {
+                    hearingRequest.getHearing().getPayment().setStatus(Status.ACTIVE);
+                } else {
+                    hearingRequest.getHearing().getPayment().setStatus(Status.DRAFTED);
+                }
+                hearingRequest.getHearing().setHearingNumber(hearingDetailsRepository.getMaxValueOfHearing(hearingRequest.getHearing().getCaseId()));
                 hearingDetailsValidator.createValidator(hearingRequest);
                 hearingEnrichmentService.enrichHearingCreateRequest(hearingRequest);
                 producer.push(ilmsConfiguration.getCreateHearingTopic(), hearingRequest);
             } else {
                 throw new CustomException(ILMSErrorConstants.INVALID_TYPE_ERROR, "CaseNumber Invalid");
             }
+
         } else {
             throw new CustomException(ILMSErrorConstants.CASE_NOT_AVAILABLE, "Case is not Available for this Hearing");
         }
