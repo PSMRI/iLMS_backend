4c00d720a55aab3ac9336af7f230ca1fb83b64f4
VaibhavJain1996 <vaibhav.jain@walkingtree.tech>
2023-02-20 16:22:24 +0530

    Update HearingUtils.java

--- municipal-services/ilms-services/src/main/java/org/ilms/util/HearingUtils.java
+++ municipal-services/ilms-services/src/main/java/org/ilms/util/HearingUtils.java
@@ -82,13 +82,13 @@ public class HearingUtils {
             }
         }
         if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getNextHearingDate())) {
-            List<String> uuids = new ArrayList<>();
-            uuids.add(hearingDetailsRequest.getRequestInfo().getUserInfo().getUuid());
-            if (commonUtils.isUserOIC(uuids, tenantId, "NextHearingDate")) {
+//            List<String> uuids = new ArrayList<>();
+//            uuids.add(hearingDetailsRequest.getRequestInfo().getUserInfo().getUuid());
+//            if (commonUtils.isUserOIC(uuids, tenantId, "NextHearingDate")) {
                 oldHearingRequest.setNextHearingDate(hearingDetailsRequest.getHearing().getNextHearingDate());
 
             }
-        }
+//        }
 //        if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getFirstHearingDate())) {
 //            List<String> uuids = new ArrayList<>();
 //            uuids.add(hearingDetailsRequest.getRequestInfo().getUserInfo().getUuid());
@@ -97,12 +97,12 @@ public class HearingUtils {
 //            }
         //}
         if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getIsPresenceRequired())) {
-            List<String> uuids = new ArrayList<>();
-            uuids.add(hearingDetailsRequest.getRequestInfo().getUserInfo().getUuid());
-            if (commonUtils.isUserOIC(uuids, tenantId, "IsPresenceRequired")) {
+//            List<String> uuids = new ArrayList<>();
+//            uuids.add(hearingDetailsRequest.getRequestInfo().getUserInfo().getUuid());
+//            if (commonUtils.isUserOIC(uuids, tenantId, "IsPresenceRequired")) {
                 oldHearingRequest.setIsPresenceRequired(hearingDetailsRequest.getHearing().getIsPresenceRequired());
             }
-        }
+//        }
 
         if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getHearingType())) {
             oldHearingRequest.setHearingType(hearingDetailsRequest.getHearing().getHearingType());
