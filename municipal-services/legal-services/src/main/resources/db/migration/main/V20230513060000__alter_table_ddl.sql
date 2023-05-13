490cb9fe6822a2d9ba886097cd1b6d182e5c6258
avanish-wtt <avanish.singh@walkingtree.tech>
2023-05-13 18:58:05 +0530

    Create V20230513060000__alter_table_ddl.sql

---
+++ municipal-services/legal-services/src/main/resources/db/migration/main/V20230513060000__alter_table_ddl.sql
@@ -0,0 +1,6 @@
+ALTER TABLE eg_lg_case
+DROP COLUMN case_sub_stage,
+DROP COLUMN registration_date;
+ALTER TABLE eg_lg_document DROP COLUMN document_type;
+ALTER TABLE eg_lg_case
+ADD COLUMN IF NOT EXISTS first_hearing_date bigint;
