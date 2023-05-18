50a3fc553ea670df2bebe3c0bca4b3e907b7965d
pallavi.rawat <pallavi.rawat@mhealth.tech>
2023-05-18 15:51:40 +0530

    Update V20230516173154__create_bridg_ddl.sql

--- municipal-services/legal-services/src/main/resources/db/migration/main/V20230516173154__create_bridg_ddl.sql
+++ municipal-services/legal-services/src/main/resources/db/migration/main/V20230516173154__create_bridg_ddl.sql
@@ -10,7 +10,7 @@ createdby character varying(64),
 createdtime bigint,
 lastmodifiedby character varying(64),
 lastmodifiedtime bigint,
-CONSTRAINT pk_eg_lg_party_advocate_bridge_id PRIMARY KEY (id),
+CONSTRAINT pk_eg_lg_party_advocate_bridge_id PRIMARY KEY (id)
 );
 
 ALTER TABLE eg_lg_case_party DROP COLUMN advocate_id;
