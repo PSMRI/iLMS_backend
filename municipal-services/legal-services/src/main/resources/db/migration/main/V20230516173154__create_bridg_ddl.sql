adcb9ba28e77172710763cfbbf745c95baa2a779
VaibhavJain1996 <vaibhav.jain@walkingtree.tech>
2023-05-18 10:31:51 +0530

    Update V20230516173154__create_bridg_ddl.sql

--- municipal-services/legal-services/src/main/resources/db/migration/main/V20230516173154__create_bridg_ddl.sql
+++ municipal-services/legal-services/src/main/resources/db/migration/main/V20230516173154__create_bridg_ddl.sql
@@ -6,10 +6,7 @@ case_id character varying(64) NOT NULL,
 party_id character varying(64) NOT NULL,
 party_type character varying(64) NOT NULL,
 advocate_id character varying(64) NOT NULL,
-CONSTRAINT pk_eg_lg_party_advocate_bridge_id PRIMARY KEY (id),
-CONSTRAINT fk_eg_lg_party_advocate_bridge_case_id FOREIGN KEY (case_id) REFERENCES eg_lg_case (id),
-CONSTRAINT fk_eg_lg_party_advocate_bridge_party_id FOREIGN KEY(party_id) REFERENCES eg_lg_case_party(id),
-CONSTRAINT fk_eg_lg_party_advocate_bridge_advocate_id FOREIGN KEY(advocate_id) REFERENCES eg_lg_advocate(id)
+CONSTRAINT pk_eg_lg_party_advocate_bridge_id PRIMARY KEY (id)
 );
 
 ALTER TABLE eg_lg_case_party DROP COLUMN advocate_id;
