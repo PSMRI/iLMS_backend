6a6370c2a325d12fd09f05e255ea8fc98c2de12e
VaibhavJain1996 <vaibhav.jain@walkingtree.tech>
2023-05-30 15:45:17 +0530

    to check null for Long objs

--- municipal-services/legal-services/src/main/java/org/legal/util/HearingUtils.java
+++ municipal-services/legal-services/src/main/java/org/legal/util/HearingUtils.java
@@ -72,16 +72,16 @@ public class HearingUtils {
         if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getHearingNumber())) {
             oldHearingRequest.setHearingNumber(hearingDetailsRequest.getHearing().getHearingNumber());
         }
-        if (hearingDetailsRequest.getHearing().getFirstHearingDate() != 0) {
+        if (Objects.nonNull(hearingDetailsRequest.getHearing().getFirstHearingDate())) {
             oldHearingRequest.setFirstHearingDate(hearingDetailsRequest.getHearing().getFirstHearingDate());
         }
         if (Objects.nonNull(hearingDetailsRequest.getHearing().getJudgeName())) {
             oldHearingRequest.setJudgeName(hearingDetailsRequest.getHearing().getJudgeName());
         }
-        if (hearingDetailsRequest.getHearing().getHearingDate() != 0) {
+        if (Objects.nonNull(hearingDetailsRequest.getHearing().getHearingDate())) {
             oldHearingRequest.setHearingDate(hearingDetailsRequest.getHearing().getHearingDate());
         }
-        if (hearingDetailsRequest.getHearing().getBusinessDate() != 0) {
+        if (Objects.nonNull(hearingDetailsRequest.getHearing().getBusinessDate())) {
             oldHearingRequest.setBusinessDate(hearingDetailsRequest.getHearing().getBusinessDate());
         }
         if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getCourtRoomNumber())) {
@@ -96,14 +96,14 @@ public class HearingUtils {
         if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getRequiredOfficer())) {
             oldHearingRequest.setRequiredOfficer(hearingDetailsRequest.getHearing().getRequiredOfficer());
         }
-        if (hearingDetailsRequest.getHearing().getAffidavitFilingDate() != 0) {
+        if (Objects.nonNull(hearingDetailsRequest.getHearing().getAffidavitFilingDate())) {
             List<String> uuids = new ArrayList<>();
             uuids.add(hearingDetailsRequest.getRequestInfo().getUserInfo().getUuid());
             if (commonUtils.isUserOIC(uuids, tenantId, "AffidavitFilingDate")) {
                 oldHearingRequest.setAffidavitFilingDate(hearingDetailsRequest.getHearing().getAffidavitFilingDate());
             }
         }
-        if (hearingDetailsRequest.getHearing().getAffidavitFilingDueDate() != 0) {
+        if (Objects.nonNull(hearingDetailsRequest.getHearing().getAffidavitFilingDueDate())) {
             oldHearingRequest.setAffidavitFilingDueDate(hearingDetailsRequest.getHearing().getAffidavitFilingDueDate());
         }
         if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getCaseNumber())) {
@@ -116,7 +116,7 @@ public class HearingUtils {
                 oldHearingRequest.setOathNumber(hearingDetailsRequest.getHearing().getOathNumber());
             }
         }
-        if (hearingDetailsRequest.getHearing().getNextHearingDate() != 0) {
+        if (Objects.nonNull(hearingDetailsRequest.getHearing().getNextHearingDate())) {
             oldHearingRequest.setNextHearingDate(hearingDetailsRequest.getHearing().getNextHearingDate());
 
         }
@@ -141,7 +141,7 @@ public class HearingUtils {
             if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getPayment().getFineImposedDate())) {
                 oldHearingRequest.getPayment().setFineImposedDate(hearingDetailsRequest.getHearing().getPayment().getFineImposedDate());
             }
-            if (hearingDetailsRequest.getHearing().getPayment().getFineDueDate() != 0) {
+            if (Objects.nonNull(hearingDetailsRequest.getHearing().getPayment().getFineDueDate())) {
                 oldHearingRequest.getPayment().setFineDueDate(hearingDetailsRequest.getHearing().getPayment().getFineDueDate());
             }
             if (!StringUtils.isEmpty(hearingDetailsRequest.getHearing().getPayment().getFineAmount())) {
