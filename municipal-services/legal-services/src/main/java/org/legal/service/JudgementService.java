484906a3f3a34e508eb45e61f3594511f6f238bc
pallavi.rawat <pallavi.rawat@mhealth.tech>
2023-05-31 14:02:40 +0530

    Update JudgementService.java

--- municipal-services/legal-services/src/main/java/org/legal/service/JudgementService.java
+++ municipal-services/legal-services/src/main/java/org/legal/service/JudgementService.java
@@ -137,6 +137,7 @@ public class JudgementService {
 
             String action = "";
             CaseRequest caseRequest = new CaseRequest();
+            JudgementRequest finalRequest;
             if (judgementRequest.getJudgement().getId() != null) {
                 JudgementSearchCriteria criteria = JudgementSearchCriteria.builder().id(Collections.singletonList(judgementRequest.getJudgement().getId()))
                         .build();
@@ -144,7 +145,7 @@ public class JudgementService {
                 if (!judgementResponse.getJudgementList().isEmpty()) {
                     List<Judgement> judgements = judgementResponse.getJudgementList();
                     Judgement oldJudgement = judgements.get(0);
-                    JudgementRequest finalRequest = judgementRepository.getMappedData(judgementRequest, oldJudgement);
+                    finalRequest = judgementRepository.getMappedData(judgementRequest, oldJudgement);
                     judgementValidator.updateValidator(finalRequest.getJudgement(), judgementRequest);
 
                     String caseId = judgementRequest.getJudgement().getCaseId();
@@ -213,7 +214,7 @@ public class JudgementService {
             } else {
                 throw new CustomException(LegalErrorConstants.INVALID_TYPE_ERROR, "Id is mandatory");
             }
-            return judgementRequest;
+            return finalRequest;
         }  catch (CustomException e) {
             throw e;
         } catch (Exception e) {
