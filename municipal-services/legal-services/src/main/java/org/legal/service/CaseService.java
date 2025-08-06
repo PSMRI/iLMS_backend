9971f26dc135815e17c23923271f1a32c656bf4c
pallavi.rawat <pallavi.rawat@walkingtree.tech>
2025-08-06 13:06:10 +0530

    Added condition for workflow status update

--- municipal-services/legal-services/src/main/java/org/legal/service/CaseService.java
+++ municipal-services/legal-services/src/main/java/org/legal/service/CaseService.java
@@ -143,6 +143,7 @@ public class CaseService {
                     if (Objects.nonNull(updatedCaseRequest.getWorkflow())) {
                         if (legalConfiguration.getIsWorkflowEnabled()) {
                             updatedCaseRequest.getWorkflow().setBusinessService(legalConfiguration.getCreateCaseWfName());
+                            workflowService.updateCaseWorkflowStatus(updatedCaseRequest);
                         }
                         hearingSearchCriteria = HearingSearchCriteria.builder().caseId(caseId).build();
                         HearingResponse hearingResponse = hearingRepository.getHearingDetails(hearingSearchCriteria);
