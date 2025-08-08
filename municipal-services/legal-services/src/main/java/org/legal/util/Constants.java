46315798450277273031850942539413cfba715f
pallavi.rawat <pallavi.rawat@walkingtree.tech>
2025-08-08 15:45:07 +0530

    hearing

--- municipal-services/legal-services/src/main/java/org/legal/util/Constants.java
+++ municipal-services/legal-services/src/main/java/org/legal/util/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final String ASSIGNED_TO_RO = "ASSIGNED_TO_RO";
     public static final String CREATE_CASE = "CREATE_CASE";
     public static final String MAIN_CASE = "mainCase";
-    public static final String HEARING_CREATED = "Hearing Pending At DEC";
+    public static final String HEARING_CREATED = "Pending At DEC";
     public static final String PENDING_AT_RO = "Pending at Reviewing Officer";
     public static final String Pending_At_DEC_for_next_hearing = "Current Hearing Pending at DEC";
     public static final String REVIEW_TO_RO = "REVIEW_TO_RO";
